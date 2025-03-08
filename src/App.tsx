1731b98a04182772f0b0b7219e8daf79858b8547
0xC0FFE2 <github1@ncloud.sbs>
2025-03-08 16:24:03 +0900

    chore : 서비스 안내 페이지 > 메인페이지로 라우팅 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import ServiceIntroduction from "./pages/ServiceIntroduction";
+import ServiceIntroduction from "./pages/DefaultIntro";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/service/intro" element={<ServiceIntroduction />} />
+        <Route path="/" element={<ServiceIntroduction />} />
       </Routes>
     </BrowserRouter>
   );
